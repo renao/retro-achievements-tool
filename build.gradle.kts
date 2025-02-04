aac85791b4eaa2a6237eaf937aef42a966d7b11c
René Siemer <me@renao.de>
2025-02-04 23:10:17 +0100

    Update to current implementation status

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 group = "de.renao"
@@ -7,9 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
+    implementation("com.github.RetroAchievements:api-kotlin:1.0.15")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     testImplementation(kotlin("test"))
 }
 
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,38 @@
 package de.renao
 
+import de.renao.models.RASettings
+import de.renao.models.RAUserProfile
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import java.io.InputStream
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+var fetcher : RAFetcher = RAFetcher("","")
+
 fun main() {
     val settings = loadSettings()
+    fetcher = RAFetcher(settings.username, settings.webApiKey)
+
+    runBlocking {
+        val renao = loadUserData("renao")
+        printUserData(renao)
     }
+}
+
+fun printUserData(userProfile: RAUserProfile) {
+    println("User => ${userProfile.name} (ID: ${userProfile.id})")
+    println("=== since ${userProfile.memberSince} ===")
+    println("*** ${userProfile.totalPoints} total points ***")
+    println("*** ${userProfile.totalTruePoints} total true points ***")
+    println("*** ${userProfile.totalSoftcorePoints} total softcore points ***")
+}
+
+suspend fun loadUserData(username: String) : RAUserProfile {
+    return fetcher.fetchUserProfile(username)
+}
+
 fun loadSettings() : RASettings {
     val settingsText = loadFromResources("ra-settings.json")
     return Json.decodeFromString<RASettings>(settingsText)
