e7823de172b7b73ef4e77034a1b7499f5eb8ffbf
René Siemer <me@renao.de>
2025-02-06 22:36:56 +0100

    User SettingsLoader, update coroutine calls, clean up

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,20 @@
+@file:OptIn(DelicateCoroutinesApi::class)
+
 package de.renao
 
-import de.renao.models.*
 import de.renao.renderers.*
-import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json
-import java.io.InputStream
+import kotlinx.coroutines.*
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-var fetcher : RAFetcher = RAFetcher("", "")
+val settings = ResourcesSettingsLoader.loadSettings()
+val fetcher : RAFetcher = RAFetcher(settings.username, settings.webApiKey)
 
 fun main() {
-    val settings = loadSettings()
-    fetcher = RAFetcher(settings.username, settings.webApiKey)
+
+    val userInfo = GlobalScope.async { fetcher.fetchUserProfile("renao") }
+    val userGameCompletion = GlobalScope.async { fetcher.fetchUserGameProgress("renao", 15935) }
 
     runBlocking {
-        val renao = fetcher.fetchUserProfile("renao")
-        UserProfileRenderer.renderProfile(renao)
-
-        val userGameCompletion = fetcher.fetchUserGameProgress("renao", 15935)
-        UserGameCompletionRenderer.renderGameCompletion(userGameCompletion, onlyMissing = false)
-
+        UserProfileRenderer.renderProfile(userInfo.await())
+        UserGameCompletionRenderer.renderGameCompletion(userGameCompletion.await(), onlyMissing = false)
     }
-}
-
-fun loadSettings() : RASettings {
-    val settingsText = loadFromResources("ra-settings.json")
-    return Json.decodeFromString<RASettings>(settingsText)
-}
-
-fun loadFromResources(fileName: String): String {
-    val inputStream: InputStream? = object {}.javaClass.getResourceAsStream("/$fileName")
-    return inputStream?.bufferedReader()?.use { it.readText() }
-        ?: throw IllegalArgumentException("File not found: $fileName")
 }
